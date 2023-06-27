3e7f648029e153ff5ee51542864eba11070aeb08
Bilal Ahmed <ranabilalrajput09@gmail.com>
2023-06-27 20:41:16 +0200

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,11 @@
 require './person'
 class Student < Person
   def initialize(
-    id:,
     age:,
     classroom:, name: 'Unknown',
     parent_permisson: true
   )
-    super(id: id, age: age, name: name, parent_permisson: parent_permisson)
+    super(age: age, name: name, parent_permisson: parent_permisson)
     @classroom = classroom
   end
 
