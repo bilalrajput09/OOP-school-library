171fbac7985eefeb2194a6f4bd9304c128f1fdf6
Bilal Ahmed <ranabilalrajput09@gmail.com>
2023-06-27 20:43:02 +0200

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -2,13 +2,12 @@ require './person'
 
 class Teacher < Person
   def initialize(
-    id:,
     age:,
     specialization:, name: 'Unknown',
     parent_permisson: true
   )
 
-    super(id: id, age: age, name: name, parent_permisson: parent_permisson)
+    super(age: age, name: name, parent_permisson: parent_permisson)
     @specialization = specialization
   end
 
