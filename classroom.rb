a0684b49d9e66aa77ab5662739441bec02f79334
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-06-28 13:31:26 +0500

    Add assosiations

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+require './student'
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    student.classroom = self
+    @students << student
+  end
+end
