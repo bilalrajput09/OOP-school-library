426d884267e348f05c147e8fc732b26b63138a9c
kayemba <kayembejonathan5@gmail.com>
2023-07-05 16:50:26 +0200

    Add teacher test

--- Test-unit/book_spec.rb
+++ Test-unit/book_spec.rb
@@ -1,9 +1,9 @@
 require_relative './book'
 
 describe Book do
-    book = Book.new('title', 'author')
+  book = Book.new('title', 'author')
      
-    context '#initialize' do
+  context '#initialize' do
     it 'should initialize a book' do
       expect(book.id).to be_kind_of Numeric
       expect(book.rentals).to be_empty
@@ -13,16 +13,16 @@ describe Book do
   end
 
   context '#add_rental' do
-  it 'should add a rental' do
-    first_rental = double('rental')
-    second_rental = double('rental')
-    allow(first_rental).to receive(:book=)
-    allow(second_rental).to receive(:book=)
+    it 'should add a rental' do
+      first_rental = double('rental')
+      second_rental = double('rental')
+      allow(first_rental).to receive(:book=)
+      allow(second_rental).to receive(:book=)
 
-    book.add_rental(first_rental)
-    book.add_rental(second_rental)
+      book.add_rental(first_rental)
+      book.add_rental(second_rental)
 
-    expect(book.rentals.length).to eql 2
+      expect(book.rentals.length).to eql 2
+    end
   end
-end
 end
