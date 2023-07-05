505cc3eb300d763d4609ef9ed8cc22645f4be001
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-07-05 17:16:21 +0500

    Fix linters

--- operations.rb
+++ operations.rb
@@ -1,31 +1,37 @@
-require "./store.rb"
+require './store'
 class Operations
   def initialize
     @store = Store.new
   end
+
   def store_books(title, author)
     books = []
-    book  = {title: title, author: author}
-    books = @store.read_file("books.json") if File.exist?("books.json")
+    book = { title: title, author: author }
+    books = @store.read_file('books.json') if File.exist?('books.json')
     books.push(book)
-    @store.write_file("books.json", books)
+    @store.write_file('books.json', books)
   end
-  def store_person(num, age , name, permission = true, specialization = nil)
+
+  # rubocop:disable Style/OptionalBooleanParameter
+  def store_person(num, age, name, permission = true, specialization = nil)
+    # rubocop:enable Style/OptionalBooleanParameter
+
     persons = []
-    persons = @store.read_file("persons.json") if File.exist?("persons.json")
-    if(num == 1)
-    person = {options: num, age: age, name: name, parent_permisson: permission }
-    else
-    person = {options: num, age: age, name: name, specialization: specialization}
-    end
+    persons = @store.read_file('persons.json') if File.exist?('persons.json')
+    person = if num == 1
+               { options: num, age: age, name: name, parent_permisson: permission }
+             else
+               { options: num, age: age, name: name, specialization: specialization }
+             end
     persons.push(person)
-    @store.write_file("persons.json", persons)
+    @store.write_file('persons.json', persons)
   end
+
   def store_rentals(date, book_id, person_id)
     rentals = []
-    rentals = store.read_file("rentals.json") if File.exist?("rental.json")
-    rental = {date: date, book_id: book_id, person_id: person_id }
+    rentals = store.read_file('rentals.json') if File.exist?('rental.json')
+    rental = { date: date, book_id: book_id, person_id: person_id }
     rentals.push(rental)
-    @store.write_file("rentals.json", rentals)
+    @store.write_file('rentals.json', rentals)
   end
 end
